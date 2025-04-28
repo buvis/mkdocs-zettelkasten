6c9ff37d53c7d65d944f67c6b29a828626bf4e14
Tomáš Bouška <tomas@buvis.net>
2025-04-28 21:18:10 +0200

    Reduce nesting in adapt_backlinks_to_page

--- mkdocs_zettelkasten/plugin/adapters/backlinks_to_page.py
+++ mkdocs_zettelkasten/plugin/adapters/backlinks_to_page.py
@@ -1,16 +1,43 @@
+import logging
+
 from mkdocs.structure.pages import Page
 
 from mkdocs_zettelkasten.plugin.services.zettel_service import ZettelService
 
+logger = logging.getLogger(
+    __name__.replace("mkdocs_zettelkasten.plugin.", "mkdocs.plugins.zettelkasten.")
+)
+
 
 def adapt_backlinks_to_page(page: Page, zettel_service: ZettelService) -> None:
-    if page.meta["is_zettel"]:
-        for link, source_zettels in zettel_service.backlinks.items():
-            for zettel in source_zettels:
-                if zettel.id == page.meta["zettel"].id:
-                    target_zettel = zettel_service.get_zettel_by_partial_path(link)
-                    if target_zettel:
-                        backlink = {}
-                        backlink["url"] = page.url
-                        backlink["title"] = page.title
-                        target_zettel.backlinks.append(backlink)
+    """
+    Update backlinks for zettels that link to the current page.
+
+    :param page: The page whose backlinks should be updated.
+    :param zettel_service: Service providing zettel and backlink information.
+    """
+    if not page.meta["is_zettel"]:
+        return
+
+    current_zettel_id = page.meta["zettel"].id
+
+    for link, source_zettels in zettel_service.backlinks.items():
+        for zettel in source_zettels:
+            if zettel.id != current_zettel_id:
+                continue
+
+            target_zettel = zettel_service.get_zettel_by_partial_path(link)
+            if not target_zettel:
+                continue
+
+            backlink = {
+                "url": page.url,
+                "title": page.title,
+            }
+            target_zettel.backlinks.append(backlink)
+            logger.debug(
+                "Found link from %s to %s. Adding it to %s's backlinks.",
+                zettel.rel_path,
+                target_zettel.rel_path,
+                target_zettel.rel_path,
+            )
