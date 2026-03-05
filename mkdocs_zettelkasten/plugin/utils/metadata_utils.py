7b64d7d88bd6e03e79e584e89ab0f77c12f9980d
Tomáš Bouška <tomas@buvis.net>
2026-03-05 12:58:07 +0100

    fix: use utf-8-sig encoding in metadata_utils to handle BOM

--- mkdocs_zettelkasten/plugin/utils/metadata_utils.py
+++ mkdocs_zettelkasten/plugin/utils/metadata_utils.py
@@ -18,7 +18,7 @@ def extract_file_metadata(filename: str, docs_dir: str) -> dict[str, Any]:
     file_path = Path(docs_dir) / filename
     logger.debug("Extracting metadata from file: %s.", file_path)
     try:
-        content = file_path.read_text(encoding="utf-8")
+        content = file_path.read_text(encoding="utf-8-sig")
         header_text, _ = parse_frontmatter(content)
         if not header_text:
             logger.warning("No YAML frontmatter found in file: %s.", file_path)
